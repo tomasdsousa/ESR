0829ba5073a43927b1b82397f959fa8168a5778b
Tomás Sousa <137288969+tomasdsousa@users.noreply.github.com>
2023-08-25 11:20:52 +0100

    Update test_all.py
    sr-inflation version

--- esr/fitting/test_all.py
+++ esr/fitting/test_all.py
@@ -16,7 +16,7 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-def chi2_fcn(x, likelihood, eq_numpy, integrated, signs):
+def chi2_fcn(x, likelihood, eq_numpy, integrated, signs, fstring, deriv1, deriv2):
     """Compute chi2 for a function
     
     Args:
@@ -42,8 +42,9 @@ def chi2_fcn(x, likelihood, eq_numpy, integrated, signs):
             elif signs[i] == '-':
                 p[i] = - 10 ** x[i]
             else:
+                print('raising inside chi2_fun')
                 raise ValueError
-    return likelihood.negloglike(p,eq_numpy, integrated=integrated)
+    return likelihood.negloglike(p,eq_numpy, fcn_i=fstring, dd1=deriv1, dd2=deriv2, integrated=integrated)
     
     
 def get_functions(comp, likelihood, unique=True):
@@ -65,7 +66,7 @@ def get_functions(comp, likelihood, unique=True):
         unifn_file = likelihood.fn_dir + "/compl_%i/unique_equations_%i.txt"%(comp,comp)
     else:
         unifn_file = likelihood.fn_dir + "/compl_%i/all_equations_%i.txt"%(comp,comp)
-    
+
     if comp>=8:
         sys.setrecursionlimit(2000 + 500 * (comp - 8))
 
@@ -102,7 +103,8 @@ def get_functions(comp, likelihood, unique=True):
     return fcn_list[data_start:data_end], data_start, data_end
     
     
-def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log_opt=False, max_param=4, Niter_params=[40,60], Nconv_params=[-5,20], test_success=False):
+def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, 
+                 log_opt=True, max_param=4, Niter_params=[40,60], Nconv_params=[-5,20]):
     """Optimise the parameters of a function to fit data
     
     The list of parameters, P, passed as Niter_params and Nconv_params compute these values, N, to be
@@ -120,32 +122,43 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
         :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in log space
         :max_param (int, default=4): The maximum number of parameters considered. This sets the shapes of arrays used.
         :Niter_params (list, default=[40, 60]): Parameters determining maximum number of parameter optimisation iterations to attempt.
-        :Nconv_params (list, default=[-5, 20]): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters. These parameters determine Nconv.
-        :test_sucess (bool, default=False): Whether to test whether the optimisation was successful using scipy's criteria
-        
+        :Nconv_params (list, default=[-5, 20]): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters. These parameters determine Nconv.        
     Returns:
         :chi2_i (float): the minimum value of -log(likelihood) (corresponding to the maximum likelihood)
         :params (list): the maximum likelihood values of the parameters
     
     """
-
     xvar, yvar = likelihood.xvar, likelihood.yvar
     
     nparam = simplifier.count_params([fcn_i], max_param)[0]
     params = np.zeros(max_param)
     
+    if "x" not in fcn_i or "exp(log" in fcn_i or "log(exp" in fcn_i or "exp(sin" in fcn_i or "sin(exp" in fcn_i: # the last 2 checks are arbitrary
+        return np.inf, params
+    if fcn_i.count('x')==fcn_i.count('exp'): # constant function
+        return np.inf, params
+    
+    print(fcn_i)
     Niter = int(np.sum(nparam ** np.arange(len(Niter_params)) * np.array(Niter_params)))
     Nconv = int(np.sum(nparam ** np.arange(len(Nconv_params)) * np.array(Nconv_params)))
     if (Nconv <= 0) or (Niter <= 0) or (Nconv > Niter):
         raise ValueError("Nconv and/or Niter have unacceptable values")
+
     
     try:
         fcn_i, eq, integrated = likelihood.run_sympify(fcn_i, tmax=tmax, try_integration=try_integration)
-            
+        
         if ("a0" in fcn_i)==False:
+
+
             Nparams = 0
             eq_numpy = sympy.lambdify(x, eq, modules=["numpy"])
-            chi2_i = likelihood.negloglike([], eq_numpy, integrated=integrated)
+            eq = sympy.sympify(fcn_i, locals={"inv": inv, "square": square, "cube": cube, "x": x})
+            derivative_expr = sympy.diff(eq, x)
+            V_1 = sympy.lambdify(x, derivative_expr, 'numpy')
+            derivative_expr2 = sympy.diff(derivative_expr, x)
+            V_2 = sympy.lambdify(x, derivative_expr2, 'numpy')
+            chi2_i = likelihood.negloglike([], eq_numpy, fcn_i=fcn_i, d1=V_1, d2=V_2, integrated=integrated)
             return chi2_i, params
 
         flag_three = False
@@ -159,13 +172,23 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
             all_a = ' '.join([f'a{i}' for i in range(nparam)])
             all_a = list(sympy.symbols(all_a, real=True))
             eq_numpy = sympy.lambdify([x] + all_a, eq, modules=["numpy"])
-        else:
+            eq = sympy.sympify(fcn_i,locals={"inv": inv, "square": square, "cube": cube, "sqrt": sqrt, "log": log, "pow": pow, "x": x, "a0": a0, "a1": a1, "a2": a2, "a3": a3})
+            derivative_expr = sympy.diff(eq, x)
+            V_1 = sympy.lambdify([x] + all_a, derivative_expr, 'numpy')
+            derivative_expr2 = sympy.diff(derivative_expr, x)
+            V_2 = sympy.lambdify([x] + all_a, derivative_expr2, 'numpy')
+        else: # 1 parameter
             eq_numpy = sympy.lambdify([x, a0], eq, modules=["numpy"])
-    
+            eq = sympy.sympify(fcn_i,locals={"inv": inv, "square": square, "cube": cube, "sqrt": sqrt, "log": log, "pow": pow, "x": x, "a0": a0})
+            derivative_expr = sympy.diff(eq, x)
+            V_1 = sympy.lambdify([x, a0], derivative_expr, 'numpy')
+            derivative_expr2 = sympy.diff(derivative_expr, x)
+            V_2 = sympy.lambdify([x, a0], derivative_expr2, 'numpy')
         bad_fun = True
         for p in itertools.product([1, -1], repeat=nparam):
             if not (np.sum(np.isnan(eq_numpy(xvar,*p)))>0):
                 bad_fun = False
+                
                 break
         
         if bad_fun:
@@ -179,18 +202,19 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
         if nparam > 2:
             flag_three = True
         
-        for j in range(Niter):
+        for j in range(Niter): 
+            """have to do the sympy stuff outdside of this loop"""
         
             if nparam > 2:
                 inpt = [np.random.uniform(pmin,pmax) for _ in range(nparam)]
-                res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None), method="BFGS", options={'maxiter': 7000})    # Default=3000
+                res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None, fcn_i, V_1, V_2), method="BFGS", options={'maxiter': 7000})    # Default=3000
             elif nparam == 2:
                 if log_opt:
                     inpt = [np.random.uniform(pmin,pmax), np.random.uniform(pmin,pmax)]           # These are now in log-space, so this is 1e-1 -- 1e1; was -10:10
-                    res_pp = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+','+']), method="BFGS")
-                    res_mp = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-','+']), method="BFGS")
-                    res_pm = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+','-']), method="BFGS")
-                    res_mm = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-','-']), method="BFGS")
+                    res_pp = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+','+'], fcn_i,  V_1, V_2), method="BFGS")
+                    res_mp = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-','+'], fcn_i ,V_1, V_2), method="BFGS")
+                    res_pm = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+','-'], fcn_i, V_1, V_2), method="BFGS")
+                    res_mm = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-','-'], fcn_i, V_1, V_2), method="BFGS")
                 
                     choose = np.argmin([res_pp['fun'], res_mp['fun'], res_pm['fun'], res_mm['fun']])
                     mult_arr = np.ones(max_param)
@@ -212,13 +236,13 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
                 else:
                     flag_three = True
                     inpt = [np.random.uniform(pmin,pmax), np.random.uniform(pmin,pmax)]
-                    res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None), method="BFGS", options={'maxiter': 5000})    # Default=3000
+                    res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None, fcn_i, V_1, V_2), method="BFGS", options={'maxiter': 5000})    # Default=3000
                     
             else:
                 if log_opt:
                     inpt = np.random.uniform(pmin,pmax)
-                    res_p = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+']), method="BFGS")
-                    res_m = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-']), method="BFGS")
+                    res_p = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['+'],fcn_i, V_1, V_2), method="BFGS")
+                    res_m = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, ['-'],fcn_i, V_1, V_2), method="BFGS")
 
                     mult_arr = np.ones(max_param)
                     if res_p['fun']<res_m['fun']:
@@ -232,9 +256,10 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
                 else:
                     flag_three = True
                     inpt = np.random.uniform(pmin,pmax)
-                    res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None), method="BFGS", options={'maxiter': 5000})
+                    res = minimize(chi2_fcn, inpt, args=(likelihood, eq_numpy, integrated, None, fcn_i, V_1, V_2), method="BFGS", options={'maxiter': 5000})
                     
-            if test_success and (not res.success):
+                
+            if not res.success:
                 continue
 
             if np.isinf(res['fun']):
@@ -248,7 +273,7 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
             if res['fun']-chi2_min < -2.:
                 count_lowest=0
 
-            # If within 0.5 of lowest, say converged to that value
+            # If within 0.5 of lowest, say converged to that value
             if abs(res['fun']-chi2_min) < 0.5:
                 count_lowest += 1
 
@@ -269,13 +294,12 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
                 # Params put in linear space and sign added back in
                 params = np.pad(10.**np.array(best.x), (0, max_param-len(best.x))) * mult_arr_best
         elif not np.isfinite(chi2_min):
-            print('\tFailed to find parameters for function:', fcn_i)
+            print('Failed to find parameters for function:', fcn_i)
                  
         # This is after all the iterations, so it's the best we have; reduced chi2
         chi2_i = chi2_min
 
     except NameError:
-        print(NameError)
         # Occurs if function produced not implemented in numpy
         raise NameError
 
@@ -294,43 +318,38 @@ def optimise_fun(fcn_i, likelihood, tmax, pmin, pmax, try_integration=False, log
         except:
             chi2_i = np.nan
             params[:] = 0.
-
+    
     except Exception as e:
-        print(e)
         return np.nan, params
 
     return chi2_i, params
     
     
-def main(comp, likelihood, tmax=5, pmin=0, pmax=3, print_frequency=50, try_integration=False, log_opt=False, Niter_params=[40,60], Nconv_params=[-5,20]):
+def main(comp, likelihood, tmax=5, pmin=-11, pmax=11, try_integration=False, 
+         log_opt=True, Niter_params=[40,60], Nconv_params=[-5,20]):
     """Optimise all functions for a given complexity and save results to file.
     
     This can optimise in log-space, with separate +ve and -ve branch (except when there are >=3 params in which case it does it in linear)
     
     The list of parameters, P, passed as Niter_params and Nconv_params compute these values, N, to be
-    N = P[0] + P[1] * nparam + P[2] * nparam ** 2 + ...
-    where nparam is the number of parameters of the function. The order of the polynomial is determined by
-    the length of P, so P can be arbirary in length.
-    
+   N = P[0] + P[1] * nparam + P[2] * nparam ** 2 + ...
+   where nparam is the number of parameters of the function. The order of the polynomial is determined by
+   the length of P, so P can be arbirary in length.
+   
     Args:
         :comp (int): complexity of functions to consider
         :likelihood (fitting.likelihood object): object containing data, likelihood functions and file paths
         :tmax (float, default=5.): maximum time in seconds to run any one part of simplification procedure for a given function
         :pmin (float, default=0.): minimum value for each parameter to considered when generating initial guess
         :pmax (float, default=3.): maximum value for each parameter to considered when generating initial guess
-        :print_frequency (int, default=50): the status of the fits will be printed every ``print_frequency`` number of iterations
         :try_integration (bool, default=False): when likelihood requires integral, whether to try to analytically integrate (True) or just numerically integrate (False)
         :log_opt (bool, default=False): whether to optimise 1 and 2 parameter cases in log space
         :Niter_params (list, default=[40, 60]): Parameters determining maximum number of parameter optimisation iterations to attempt.
-        :Nconv_params (list, default=[-5, 20]): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters. These parameters determine Nconv.
-        
+        :Nconv_params (list, default=[-5, 20]): If we find Nconv solutions for the parameters which are within a logL of 0.5 of the best, we say we have converged and stop optimising parameters. These parameters determine Nconv.        
     Returns:
         None
     
     """
-    
-    if rank == 0:
-        print('\nRunning fits', flush=True)
 
     fcn_list_proc, _, _ = get_functions(comp, likelihood)
     
@@ -340,10 +359,10 @@ def main(comp, likelihood, tmax=5, pmin=0, pmax=3, print_frequency=50, try_integ
     chi2 = np.zeros(len(fcn_list_proc))     # This is now only for this proc
     params = np.zeros([len(fcn_list_proc), max_param])
     for i in range(len(fcn_list_proc)):           # Consider all possible complexities
-        if rank == 0 and ((i == 0) or ((i+1) % print_frequency == 0)):
-            print(f'{i+1} of {len(fcn_list_proc)}', flush=True)
+        if rank == 0:
+            print(rank, i, len(fcn_list_proc), flush=True)
         try:
-            with simplifier.time_limit(tmax):
+            with simplifier.time_limit(100):
                 try:
                     chi2[i], params[i,:] = optimise_fun(fcn_list_proc[i], 
                                                     likelihood, 
@@ -353,7 +372,7 @@ def main(comp, likelihood, tmax=5, pmin=0, pmax=3, print_frequency=50, try_integ
                                                     try_integration=try_integration,
                                                     log_opt=log_opt,
                                                     max_param=max_param,
-                                                    Niter_params=Niter_params,
+                                                   Niter_params=Niter_params,
                                                     Nconv_params=Nconv_params)
                 except NameError:
                     if try_integration:
@@ -365,7 +384,7 @@ def main(comp, likelihood, tmax=5, pmin=0, pmax=3, print_frequency=50, try_integ
                                                     try_integration=False,
                                                     log_opt=log_opt,
                                                     max_param=max_param,
-                                                    Niter_params=Niter_params,
+                                                   Niter_params=Niter_params,
                                                     Nconv_params=Nconv_params)
                     else:
                         raise NameError
